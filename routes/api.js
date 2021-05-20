4b985ac1fd2cc0674950a5f05e024e17f5db21a4
Jake <jakech617@gmail.com>
2021-05-20 12:18:28 -0400

    added get request for workoutes in range

--- routes/api.js
+++ routes/api.js
@@ -3,13 +3,6 @@ const router = require("express").Router();
 
 router.get("/api/workouts", (req, res) => {
     db.Workout.find({}).then(dbWorkout => {
-        dbWorkout.forEach(workout => {
-            let total = 0;
-            workout.exercises.forEach(e => {
-                total += e.duration;
-            });
-            workout.totalDuration = total;
-        });
         res.json(dbWorkout);
     }).catch(err => {
         res.json(err);
@@ -36,6 +29,14 @@ router.put("/api/workouts/:id", (req, res) => {
         }).catch(err => {
             res.json(err);
         });
+});
+
+router.get("/api/workouts/range", (req, res) => {
+    db.Workout.find({}).then(dbWorkout => {
+        res.json(dbWorkout);
+    }).catch(err => {
+        res.json(err);
+    });
 });
 
 module.exports = router;
