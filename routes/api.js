ea2fdaaa086a51c9eaf8ece6c7150a3dfe8f1ee1
Jake <jakech617@gmail.com>
2021-05-20 11:53:57 -0400

    get workouts working, added post request to add a new workout

--- routes/api.js
+++ routes/api.js
@@ -2,7 +2,6 @@ const db = require("../models");
 const router = require("express").Router();
 
 router.get("/api/workouts", (req, res) => {
-
     db.Workout.find({}).then(dbWorkout => {
         dbWorkout.forEach(workout => {
             let total = 0;
@@ -15,6 +14,28 @@ router.get("/api/workouts", (req, res) => {
     }).catch(err => {
         res.json(err);
     });
+});
+
+router.post("/api/workouts", ({ body }, res) => {
+    db.Workout.create(body).then((dbWorkout => {
+        res.json(dbWorkout);
+    })).catch(err => {
+        res.json(err);
+    });
+});
+
+router.put("/api/workouts/:id", (req, res) => {
+    db.Workout.findOneAndUpdate(
+        { _id: req.params.id },
+        {
+            $inc: { totalDuration: req.body.duration },
+            $push: { exercises: req.body }
+        },
+        { new: true }).then(dbWorkout => {
+            res.json(dbWorkout);
+        }).catch(err => {
+            res.json(err);
+        });
 });
 
 module.exports = router;
